63e306b442e279bb6b6c507c642cf3e9d046ad00
111111-glitch <ngigimelissa@gmail.com>
2024-03-18 12:27:37 +0300

    complete

--- lib/models.py
+++ lib/models.py
@@ -1,9 +1,25 @@
-#!/usr/bin/env python3
+# models.py
 
-from sqlalchemy import (Column, String, Integer)
+from sqlalchemy import create_engine, func
+from sqlalchemy import ForeignKey, Table, Column, Integer, String, DateTime, MetaData
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
+convention = {
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+}
+metadata = MetaData(naming_convention=convention)
+
+Base = declarative_base(metadata=metadata)
+
+dog = Table(
+    'dogs',
+    Base.metadata,
+    Column('id', Integer(), primary_key=True),
+    Column('name', String()),
+    Column('breed', String()),
+    extend_existing=True,
+)
 
 class Dog(Base):
     __tablename__ = 'dogs'
@@ -11,3 +27,8 @@ class Dog(Base):
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     breed = Column(String())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
+
+    def __repr__(self):
+        return f"Dog(name='{self.name}', breed='{self.breed}')"
